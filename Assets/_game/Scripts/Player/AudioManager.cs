197e07d800fcfed5c97fd0b62aa5b1e5201c0ab6
Daniel Henshaw <danielhenshaw03@gmail.com>
2022-05-10 15:07:12 +0100

    Fixed footstep/sliding sounds
    Fixed bug that made footstep/slide sounds play when player wasn't grounded.

--- Assets/_game/Scripts/Player/AudioManager.cs
+++ Assets/_game/Scripts/Player/AudioManager.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
+    private PlayerController _playerController;
+    
     [SerializeField] private AudioSource footstepSource;
 
     [SerializeField] private AudioClip[] footsteps;
     [SerializeField] private AudioClip[] slide;
+    
+    private void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
 
     public void PlayFootstep()
     {
-        footstepSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length - 1)]);
+        if(_playerController.isGrounded || _playerController.isWallRunning) footstepSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length - 1)]);
     }
 
     public void PlaySlide()
     {
-        footstepSource.PlayOneShot(slide[Random.Range(0, slide.Length - 1)]);
+        if(_playerController.isGrounded || _playerController.isWallRunning) footstepSource.PlayOneShot(slide[Random.Range(0, slide.Length - 1)]);
     }
 }
